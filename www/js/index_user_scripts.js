2605ca2fcf73442736b0c8e8d2166a5e682e0382
dianaros3 <nuruldiana91@gmail.com>
2017-10-02 20:02:29 +0800

    init

---
+++ www/js/index_user_scripts.js
@@ -0,0 +1,144 @@
+/*jshint browser:true */
+/*global $ */(function()
+{
+ "use strict";
+ /*
+   hook up event handlers 
+ */
+ function register_event_handlers()
+ {
+    
+    
+     /* button  Back */
+    
+    
+        /* button  .uib_w_1 */
+    $(document).on("click", ".uib_w_1", function(evt)
+    {
+         /*global activate_page */
+         activate_page("#info"); 
+         return false;
+    });
+    
+        /* button  camera */
+    $(document).on("click", ".uib_w_1", function(evt)
+    {
+         /*global activate_page */
+         activate_page("#camera"); 
+         return false;
+    });
+    
+        /* button  .uib_w_11 */
+    $(document).on("click", ".uib_w_11", function(evt)
+    {
+         /*global activate_page */
+         activate_page("#mainpage"); 
+         return false;
+    });
+    
+        /* button  .uib_w_12 */
+    $(document).on("click", ".uib_w_12", function(evt)
+    {
+         /*global uib_sb */
+         /* Other possible functions are: 
+           uib_sb.open_sidebar($sb)
+           uib_sb.close_sidebar($sb)
+           uib_sb.toggle_sidebar($sb)
+            uib_sb.close_all_sidebars()
+          See js/sidebar.js for the full sidebar API */
+        
+         uib_sb.toggle_sidebar($(".uib_w_13"));  
+         return false;
+    });
+    
+        /* button  IFTITAH / ISTIFTAH */
+    $(document).on("click", ".uib_w_14", function(evt)
+    {
+         /*global activate_page */
+         activate_page("#Iftitah"); 
+         return false;
+    });
+    
+        /* button  IFTITAH / ISTIFTAH */
+    $(document).on("click", ".uib_w_9", function(evt)
+    {
+         /*global activate_page */
+         activate_page("#Iftitah"); 
+         return false;
+    });
+    
+        /* button  Iftitah / Istiftah */
+    $(document).on("click", ".uib_w_9", function(evt)
+    {
+         /*global activate_page */
+         activate_page("#Iftitah"); 
+         return false;
+    });
+    
+        /* button  .uib_w_78 */
+    $(document).on("click", ".uib_w_78", function(evt)
+    {
+         /*global activate_page */
+         activate_page("#mainpage"); 
+         return false;
+    });
+    
+        /* button  .uib_w_80 */
+    $(document).on("click", ".uib_w_80", function(evt)
+    {
+         /*global activate_page */
+         activate_page("#mainpage"); 
+         return false;
+    });
+    
+        /* button  .uib_w_110 */
+    $(document).on("click", ".uib_w_110", function(evt)
+    {
+         /*global activate_page */
+         activate_page("#mainpage"); 
+         return false;
+    });
+    
+        /* button  .uib_w_127 */
+    $(document).on("click", ".uib_w_127", function(evt)
+    {
+         /*global activate_page */
+         activate_page("#mainpage"); 
+         return false;
+    });
+    
+        /* button  Isti’adzah / Ta’awwudz */
+    $(document).on("click", ".uib_w_15", function(evt)
+    {
+         /*global activate_page */
+         activate_page("#Istiadzah"); 
+         return false;
+    });
+    
+        /* button  Ruku’ */
+    $(document).on("click", ".uib_w_16", function(evt)
+    {
+         /*global activate_page */
+         activate_page("#ruku"); 
+         return false;
+    });
+    
+        /* button  I’tidal */
+    $(document).on("click", ".uib_w_17", function(evt)
+    {
+         /*global activate_page */
+         activate_page("#Itidal"); 
+         return false;
+    });
+    
+        /* button  Prostration (Sujood) */
+    $(document).on("click", ".uib_w_18", function(evt)
+    {
+         /*global activate_page */
+         activate_page("#Sujud"); 
+         return false;
+    });
+    
+    }
+ document.addEventListener("app.Ready", register_event_handlers, false);
+})();
